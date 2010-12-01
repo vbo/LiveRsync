2796d9a197f6006a3079fefcf9f8c5134a081db7
Borodin Vadim <vbo@rentsoft.ru>
2010-12-01 23:59:42 +0300

    readme + minor changes

--- liversync.py
+++ liversync.py
@@ -1,16 +1,18 @@
+#!/usr/bin/env python
+
 import os
 import ConfigParser
 
 config = ConfigParser.ConfigParser()
-config.readfp(file('config.example.ini'))
+config.readfp(file('config.ini'))
 
 with file('/tmp/LiveRsync.pid', 'w') as pidFile:
     pidFile.write(str(os.getpid()))
 
 while True:
-    for folder in config.sections():
-        source = config.get(folder, 'source')
-        dest = config.get(folder, 'dest')
+    for directory in config.sections():
+        source = config.get(directory, 'source')
+        dest = config.get(directory, 'dest')
         command = 'rsync -rlptz -e ssh --out-format="[%t] %o:%f" --delete '  + source + ' ' + dest;
         os.system(command)
         
