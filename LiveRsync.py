537fdc845b0a19fc51dfc3a90790c33840512d51
Borodin Vadim <borodin.vadim@gmail.com>
2010-12-11 20:05:26 +0300

    refactoring (preparing to special opts support)

--- LiveRsync.py
+++ LiveRsync.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
+"""Real time directory synchronization tool based on rsync"""
+PROG = 'LiveRsync'
+VERSION = 0.42
 
 from ConfigParser import ConfigParser
 import subprocess
+import os
+import sys
+import signal
+import shutil
 
-import config
+class Config:
+    __rsh = 'ssh -q -o PasswordAuthentication=no'
+    baseCommand = "rsync -rlptzq -e '%s' --delete" % __rsh
+    workingDir = os.path.expanduser('~/.liversync/')
+    pidFileName = 'pidfile.pid'
+    projectsFileName = 'projects.ini'
 
-class Daemon:
-    """Real time directory synchronization tool based on rsync"""
-    VERSION = 'LiveRsync 0.4'
-    
+class Synchronizer:
     def __init__(self):
         self.commands = {}
         self.prepareCommands()
 
     def prepareCommands(self):
         projects = ConfigParser()
-        path = config.workingDir + config.projectsFileName
+        path = Config.workingDir + Config.projectsFileName
         try:
             projects.readfp(file(path))
         except IOError:
@@ -24,15 +33,14 @@ class Daemon:
         for project in projects.sections():
             source = projects.get(project, 'source')
             dest = projects.get(project, 'dest')
-            self.commands[project] = ' '.join((config.baseCommand, source, dest))
-        
+            self.commands[project] = ' '.join((Config.baseCommand, source, dest))
 
     def syncAll(self):
         for project, command in self.commands.items():
             process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             yield project, process
             
-    def mainLoop(self):
+    def loop(self):
         while True:
             processes = []
             for project, process in self.syncAll():
@@ -46,7 +54,77 @@ class Daemon:
 class Warning (Exception):
     pass
 
+class Controller:
+    def start(self):
+        if self.__getPid():
+            print "Seems allready running or killed manually"
+            exit();
+
+        errors = 0
+        print "Checking projects. Be patient..."
+        try:
+            for project, process in Synchronizer().syncAll():
+                print '{0!r:<15}\t'.format(project),
+                result = process.wait()
+                errors += result
+                if result == 0:
+                    print 'Ok'
+                else:
+                    print 'Error'
+            if errors:
+                raise Warning('There were some errors.\n' +
+                'Check out your {dir} and try again'.format(
+                    dir=Config.workingDir + Config.projectsFileName))
+        except Warning as e:
+            print e
+            exit()
+
+        dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+        pid = subprocess.Popen(os.path.join(dir, 'LiveRsync.py')).pid
+        self.__createPidFile(pid)
+        print "LiveRsync daemon started with pid", pid
+
+
+    def kill(self):
+        pid = self.__getPid()
+        if pid:
+            try:
+                os.kill(pid, signal.SIGKILL)
+                print "{0} daemon (pid {1}) successfully killed".format(PROG, pid)
+            except OSError:
+                print "Seems allready killed"
+            self.__deletePidFile()
+        else:
+            print 'Seems not running'
+
+    def install(self):
+        try:
+            os.makedirs(Config.workingDir)
+            shutil.copyfile('projects-example.ini', Config.workingDir + Config.projectsFileName)
+            print 'LiveRsync successfully installed'
+        except OSError:
+            print "Seems already installed. Remove {0} to uninstall".format(Config.workingDir)
+
+    def restart(self):
+        self.kill()
+        self.start()
+
+    def __createPidFile(self, pid):
+        with file(Config.workingDir + Config.pidFileName, 'w') as pidFile:
+            pidFile.write(str(pid))
+
+    def __deletePidFile(self):
+        os.remove(Config.workingDir + Config.pidFileName)
+
+    def __getPid(self):
+        try:
+            f = file(Config.workingDir + Config.pidFileName)
+            pid = int(f.readline())
+            return pid
+        except IOError:
+            return False
+
 if __name__ == '__main__':
-    Daemon().mainLoop()
+    Synchronizer().loop()
 
         
--- control.py
+++ control.py
@@ -1,92 +1,14 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import subprocess
-import signal
-import shutil
 import argparse
-
-import config
 import LiveRsync
 
-toolName = 'LiveRsync'
-class Controller:    
-    def start(self):
-        if self.__getPid():
-            print "Seems allready running or killed manually"
-            exit();
-
-        errors = 0
-        print "Checking projects. Be patient..."
-        try:
-            for project, process in LiveRsync.Daemon().syncAll():
-                print '{0!r:<15}\t'.format(project),
-                result = process.wait()
-                errors += result
-                if result == 0:
-                    print 'Ok'
-                else:
-                    print 'Error'
-        except LiveRsync.Warning as e:
-            print e
-            exit()
-
-        if errors:
-            print 'There were some errors.'
-            print 'Check out your {dir} and try again'.format(dir=config.workingDir + config.projectsFileName);
-            exit();
-
-        dir = os.path.dirname(os.path.realpath(sys.argv[0]))
-        pid = subprocess.Popen(os.path.join(dir, 'LiveRsync.py')).pid
-        self.__createPidFile(pid)
-        print "{0} started with pid {1}".format(toolName, pid)
-        
-
-    def kill(self):
-        pid = self.__getPid()
-        if pid:
-            try:
-                os.kill(pid, signal.SIGKILL)
-                print "{0} process (pid {1}) successfully killed".format(toolName, pid)
-            except OSError:
-                print "Seems allready killed"
-            self.__deletePidFile()
-        else:
-            print 'Seems not running'
-
-    def install(self):
-        try:
-            os.makedirs(config.workingDir)
-            shutil.copyfile('projects-example.ini', config.workingDir + config.projectsFileName)
-            print '{0} installed'.format(toolName)
-        except OSError:
-            print "Seems already installed. Remove {0} to uninstall".format(config.workingDir)
-
-    def restart(self):
-        self.kill()
-        self.start()
-
-    def __createPidFile(self, pid):
-        with file(config.workingDir + config.pidFileName, 'w') as pidFile:
-            pidFile.write(str(pid))
-
-    def __deletePidFile(self):
-        os.remove(config.workingDir + config.pidFileName)
-
-    def __getPid(self):
-        try:
-            f = file(os.path.expanduser(config.workingDir + config.pidFileName))
-            pid = int(f.readline())
-            return pid
-        except IOError:
-            return False
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description=LiveRsync.Daemon.__doc__)
-    parser.add_argument('--version', action='version', version=LiveRsync.Daemon.VERSION)
+    parser = argparse.ArgumentParser(description=LiveRsync.__doc__)
+    parser.add_argument('--version', action='version',
+                        version='{0} {1}'.format(LiveRsync.PROG, LiveRsync.VERSION))
     group = parser.add_mutually_exclusive_group()
-    controller = Controller()
+    controller = LiveRsync.Controller()
     for field in dir(controller):
         action = getattr(controller, field)
         if callable(action) and not field.startswith('_'):
